27b56896db270d8670252c8dc37ee6b619d164f7
Jeanclyde Cruz <jeanclydecruz@gmail.com>
2025-06-07 18:51:11 -1000

    Simplify home page and add AI call endpoint

--- RaspberryZero2_Portal/src/app/app.component.ts
+++ RaspberryZero2_Portal/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RaspberryZero2_Portal';
+  constructor(private http: HttpClient) {}
+
+  startCall(): void {
+    this.http.post<{ message: string }>('/api/start-call', {}).subscribe({
+      next: (res) => console.log(res.message),
+      error: (err) => console.error('Error starting call', err),
+    });
+  }
 }
